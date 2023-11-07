eec7791da3a6a34a4551b6fa74e37761c82c5252
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-11-07 14:49:26 -0500

    navbar

---
+++ src/components/SearchInput.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="flex justify-center mt-8 gap-3">
+  <input
+    autocomplete="off"
+    class="p-2 rounded-md w-80"
+    type="text" 
+    placeholder="How to make Tacos"
+    name="search" 
+    id="search"
+  >
+  <button class="text-white rounded-md px-5 bg-gradient-to-tr from-pink-500 via-purple-500 to-indigo-500 bg-[position:_0%_0%] hover:bg-[position:_100%_100%] bg-[size:_200%] w-40 transition-all duration-500">
+    Search
+  </button>
+</div>
